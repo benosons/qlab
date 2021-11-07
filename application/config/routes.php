e2fc6f07568dde420d9a9ea912b50081b7e80ed9
retno sonjaya <beno.sons@khansia.co.id>
2021-11-07 18:59:13 +0700

    update

--- application/config/routes.php
+++ application/config/routes.php
@@ -57,8 +57,4 @@ $route['translate_uri_dashes'] = FALSE;
 
 $route['^(logout|loginAdmin|Admin|register|cekusername)(/:any)?$'] = "auth/$0";
 $route['^(dashboard|saveUser|listuser|updateUser|formUser|listDataUser|deleteUser|dataDetailUser|loadkota|hitungAll|profile|loaduser|infodata|listdatabanner|listdatabanneruser|loadsetting|simpansetting|savebanner|updatebanner|deletebanner|updateprofile)(/:any)?$'] = "sys/$0";
-$route['^(listaduan|listDataAduanGlobal|listDataAduan|cekBalasan|saveAduan|saveBalasan|updateAduan|hitungStatus|hitungPesanBaru|closeAduan|new|reply|close)(/:any)?$'] = "aduan/$0";
-$route['^(listtelevisi|listradio|listDataSiaran|loadsiaran|loadkota_lembaga|loadLP|updateLembaga|lembaga|saveLembaga)(/:any)?$'] = "siaran/$0";
-$route['^(listvideo|addvideo|listDataVideo|deletevideo|updatevideo)(/:any)?$'] = "video/$0";
-$route['editlembaga'] = "siaran/editlembaga";
-$route['listaduan'] = "aduan/listaduan";
+$route['^(listpengajuan|addpengajuan|listujilab|addlaporan)(/:any)?$'] = "view/$0";
